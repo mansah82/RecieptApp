a81205284163417863eff244162858e2babed4b0
Robin <90704273+RobinXia99@users.noreply.github.com>
2022-11-23 00:57:01 +0100

    Detail page (#5)
    * Added DetailPage
    
    * Imports

---
+++ lib/DetailPage.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:recipe_app/models/recipe.dart';
+
+class DetailPage extends StatefulWidget {
+  final Recipe recipe;
+  DetailPage({super.key, required this.recipe});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.name),
+      ),
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Image.network(
+                    widget.recipe.image,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  Align(
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: GestureDetector(
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5.0),
+                              color: Colors.white),
+                          child: Icon(Icons.favorite_outline_outlined,
+                              color: Colors.black),
+                        ),
+                        onTap: () {
+                          print('Favorited');
+                        },
+                      ),
+                    ),
+                    alignment: Alignment.bottomRight,
+                  )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.recipe.name,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Colors.black),
+                    )
+                  ],
+                ),
+              ),
+              Divider(
+                height: 1.5,
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Categories',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Colors.black),
+                    ),
+                    ...widget.recipe.labels.map((label) => Text(label)),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 1.5,
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Description',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      widget.recipe.description,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 16,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 1.5,
+                thickness: 0.5,
+                color: Colors.grey,
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Ingredients',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Colors.black),
+                    ),
+                    ...widget.recipe.ingredients
+                        .map((ingredient) => Text(ingredient)),
+                  ],
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+}
