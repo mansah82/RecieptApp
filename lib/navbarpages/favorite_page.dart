26e76b331b1e2d75d70677b10779a1fa18f9c847
alizaeri <90989935+alizaeri@users.noreply.github.com>
2022-11-23 01:24:23 +0100

    add to favorit

--- lib/navbarpages/favorite_page.dart
+++ lib/navbarpages/favorite_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_app/DetailPage.dart';
 import 'package:recipe_app/models/recipe.dart';
 import 'package:recipe_app/navbarpages/add_recipe_page.dart';
 
@@ -87,11 +88,31 @@ class _FavoriteState extends State<Favorite> {
 
 class MyButton extends StatefulWidget {
   final Recipe recipe;
+  final auth = FirebaseAuth.instance.currentUser!.uid;
 
-  const MyButton({
+  MyButton({
     Key? key,
     required this.recipe,
   }) : super(key: key);
+  void _saveRecipeToUserSubcollection({
+    required Recipe recipe,
+  }) async {
+    final myRecipe = Recipe(
+      id: recipe.id,
+      image: recipe.image,
+      createdBy: recipe.createdBy,
+      name: recipe.name,
+      description: recipe.description,
+      ingredients: recipe.ingredients,
+      labels: recipe.labels,
+    );
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(auth)
+        .collection('favorite')
+        .doc(recipe.id)
+        .set(myRecipe.toMap());
+  }
 
   void _deleteDoc({
     required Recipe recipe,
@@ -114,69 +135,75 @@ class _MyButtonState extends State<MyButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 239, 127, 107),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-            child: CircleAvatar(
-              //=> wight strok
-              radius: 75,
-              backgroundColor: Color.fromARGB(255, 255, 255, 255),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => DetailPage(
+                recipe: widget.recipe,
+              ))),
+      child: Container(
+        color: const Color.fromARGB(255, 239, 127, 107),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
               child: CircleAvatar(
-                radius: 72,
-                backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
+                //=> wight strok
+                radius: 75,
+                backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                child: CircleAvatar(
+                  radius: 72,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
 
-                // NetworkImage(
-                //   '${firebaseUser["addressImage"]}'),
+                  // NetworkImage(
+                  //   '${firebaseUser["addressImage"]}'),
+                ),
               ),
             ),
-          ),
-          Text(
-            widget.recipe.name,
-            maxLines: 2,
-            overflow: TextOverflow.fade,
-            style: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 8, 8),
-                child: SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        pressAttention = !pressAttention;
-                        widget._deleteDoc(recipe: widget.recipe);
+            Text(
+              widget.recipe.name,
+              maxLines: 2,
+              overflow: TextOverflow.fade,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 8, 8),
+                  child: SizedBox(
+                    height: 40,
+                    width: 40,
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          pressAttention = !pressAttention;
+                          widget._deleteDoc(recipe: widget.recipe);
 
-                        /*
-                        widget._saveRecipeToUserSubcollection(
-                            recipe: widget.recipe);
-                            */
-                      });
-                    },
-                    icon: const ImageIcon(
-                      AssetImage("assets/icons/delete.png"),
-                      color: Color.fromARGB(255, 249, 245, 246),
-                      //Color.fromARGB(255, 239, 61, 100),
+                          /*
+                          widget._saveRecipeToUserSubcollection(
+                              recipe: widget.recipe);
+                              */
+                        });
+                      },
+                      icon: const ImageIcon(
+                        AssetImage("assets/icons/delete.png"),
+                        color: Color.fromARGB(255, 249, 245, 246),
+                        //Color.fromARGB(255, 239, 61, 100),
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
-          )
-        ],
-      ), // Use the fullName property of each item
+                )
+              ],
+            )
+          ],
+        ), // Use the fullName property of each item
+      ),
     );
   }
 }
--- lib/navbarpages/my_recipe_page.dart
+++ lib/navbarpages/my_recipe_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_app/DetailPage.dart';
 import 'package:recipe_app/models/recipe.dart';
 import 'package:recipe_app/models/user_model.dart';
 import 'package:recipe_app/navbarpages/add_recipe_page.dart';
@@ -138,67 +139,73 @@ class _MyButtonState extends State<MyButton> {
   @override
   Widget build(BuildContext context) {
     print(widget.recipe.id);
-    return Container(
-      color: const Color.fromARGB(255, 239, 127, 107),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-            child: CircleAvatar(
-              //=> wight strok
-              radius: 75,
-              backgroundColor: Color.fromARGB(255, 255, 255, 255),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => DetailPage(
+                recipe: widget.recipe,
+              ))),
+      child: Container(
+        color: const Color.fromARGB(255, 239, 127, 107),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
               child: CircleAvatar(
-                radius: 72,
-                backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
+                //=> wight strok
+                radius: 75,
+                backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                child: CircleAvatar(
+                  radius: 72,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
 
-                // NetworkImage(
-                //   '${firebaseUser["addressImage"]}'),
+                  // NetworkImage(
+                  //   '${firebaseUser["addressImage"]}'),
+                ),
               ),
             ),
-          ),
-          Text(
-            widget.recipe.name,
-            maxLines: 2,
-            overflow: TextOverflow.fade,
-            style: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 8, 8),
-                child: SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        pressAttention = !pressAttention;
-                        widget._saveRecipeToUserSubcollection(
-                            recipe: widget.recipe);
-                      });
-                    },
-                    icon: ImageIcon(
-                      const AssetImage("assets/icons/fav.png"),
-                      color: (pressAttention)
-                          ? const Color.fromARGB(255, 243, 99, 135)
-                          : const Color.fromARGB(255, 249, 245, 246),
-                      //Color.fromARGB(255, 239, 61, 100),
+            Text(
+              widget.recipe.name,
+              maxLines: 2,
+              overflow: TextOverflow.fade,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 8, 8),
+                  child: SizedBox(
+                    height: 40,
+                    width: 40,
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          pressAttention = !pressAttention;
+                          widget._saveRecipeToUserSubcollection(
+                              recipe: widget.recipe);
+                        });
+                      },
+                      icon: ImageIcon(
+                        const AssetImage("assets/icons/fav.png"),
+                        color: (pressAttention)
+                            ? const Color.fromARGB(255, 243, 99, 135)
+                            : const Color.fromARGB(255, 249, 245, 246),
+                        //Color.fromARGB(255, 239, 61, 100),
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
-          )
-        ],
-      ), // Use the fullName property of each item
+                )
+              ],
+            )
+          ],
+        ), // Use the fullName property of each item
+      ),
     );
   }
 }
