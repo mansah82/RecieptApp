93e8ca6c0516805b2b1d750a77a34bea2e8672bc
RobinXia99 <robin.xia@iths.se>
2022-11-23 10:33:24 +0100

    Fixes

--- lib/navbarpages/my_recipe_page.dart
+++ lib/navbarpages/my_recipe_page.dart
@@ -16,8 +16,6 @@ class MyRecipe extends StatefulWidget {
 
 class _MyRecipeState extends State<MyRecipe> {
   int? tappedIndex;
-
-
   final currentUser = FirebaseAuth.instance.currentUser!.uid;
 
   Stream<List<Recipe>> getRecipesStream() {
@@ -27,14 +25,12 @@ class _MyRecipeState extends State<MyRecipe> {
         .map((event) {
       List<Recipe> recipes = [];
       for (var document in event.docs) {
-
         if (Recipe.fromMap(document.data()).createdBy == currentUser) {
           recipes.add(Recipe.fromMap(document.data()));
         }
 
         recipes.add(Recipe.fromMap(document.data()));
         print(recipes[0].ingredients);
-
       }
 
       return recipes;
@@ -71,7 +67,6 @@ class _MyRecipeState extends State<MyRecipe> {
                         (size.height - kToolbarHeight - 10) / 3;
                     final double itemWidth = size.width / 2;
 
-
                     return GridView(
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 2,
@@ -81,20 +76,9 @@ class _MyRecipeState extends State<MyRecipe> {
                       ),
                       primary: false,
                       shrinkWrap: true,
-
                       children: <Widget>[
                         ...recipesList.map((recipe) => MyButton(recipe: recipe))
                       ],
-
-                      children: List<Widget>.generate(
-                          recipesList.length, // same length as the data
-                          (index) {
-                        return MyButton(
-                          recipe: recipesList[index],
-                        );
-                        //gridViewTile(recipesList, index);
-                      }),
-
                     );
                   }
                   return const Center(child: Text("check your connection"));
@@ -161,86 +145,6 @@ class _MyRecipeState extends State<MyRecipe> {
                     icon: ImageIcon(
                       AssetImage("assets/icons/fav.png"),
                       color: tappedIndex == index
-                          ? Color.fromARGB(255, 249, 245, 246)
-                          : Color.fromARGB(255, 243, 99, 135),
-                      //Color.fromARGB(255, 239, 61, 100),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      ), // Use the fullName property of each item
-    );
-  }
-}
-
-class MyButton extends StatefulWidget {
-  final Recipe recipe;
-
-  const MyButton({
-    Key? key,
-    required this.recipe,
-  }) : super(key: key);
-
-  @override
-  State<MyButton> createState() => _MyButtonState();
-}
-
-class _MyButtonState extends State<MyButton> {
-  // Default to non pressed
-  bool pressAttention = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 239, 127, 107),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
-            child: CircleAvatar(
-              //=> wight strok
-              radius: 75,
-              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-              child: CircleAvatar(
-                radius: 72,
-                backgroundImage: NetworkImage(widget.recipe.image!),
-
-                // NetworkImage(
-                //   '${firebaseUser["addressImage"]}'),
-              ),
-            ),
-          ),
-          Text(
-            widget.recipe.name,
-            maxLines: 2,
-            overflow: TextOverflow.fade,
-            style: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 8, 8),
-                child: SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        pressAttention = !pressAttention;
-                      });
-                    },
-                    icon: ImageIcon(
-                      AssetImage("assets/icons/fav.png"),
-                      color: pressAttention
                           ? Color.fromARGB(255, 249, 245, 246)
                           : Color.fromARGB(255, 243, 99, 135),
                       //Color.fromARGB(255, 239, 61, 100),
