54310d9048f39b183e18aa7a891e633b5cefcada
Robin <90704273+RobinXia99@users.noreply.github.com>
2022-11-24 15:29:19 +0100

    Android nav (#8)
    * Added DetailPage
    
    * Imports
    
    * Seperate nav for ios and android
    
    * Filter branch (#6)
    
    * gridViewUsingStreamBuilder
    
    * sista
    
    * filter done, showing it and rendering not done
    
    * Showing only user recipes
    
    * Backup Filter
    
    Co-authored-by: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Co-authored-by: mansah82 <manne.sahlin@iths.se>
    
    Co-authored-by: alizaeri <90989935+alizaeri@users.noreply.github.com>
    Co-authored-by: mansah82 <manne.sahlin@iths.se>

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,15 @@ import 'package:recipe_app/models/recipe.dart';
 import 'package:recipe_app/DetailPage.dart';
 
 
+import 'dart:io' show Platform;
+
+
+import 'package:recipe_app/navbarpages/GridPage.dart';
+
+import 'package:recipe_app/models/recipe.dart';
+import 'package:recipe_app/DetailPage.dart';
+
+
 import 'package:recipe_app/navbarpages/favorite_page.dart';
 import 'package:recipe_app/navbarpages/my_recipe_page.dart';
 import 'package:recipe_app/navbarpages/grid_page.dart';
@@ -35,96 +44,224 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 247, 88, 88),
-        title: Text(
-          "Recipe App",
-          style: TextStyle(color: Colors.white),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () => null,
-              icon: GestureDetector(
-                child: PopupMenuButton<int>(
-                  elevation: 2,
-                  color: Color.fromARGB(173, 251, 251, 251),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  itemBuilder: (context) => [
-                    const PopupMenuItem(
-                      value: 1,
-                      child: Center(
-                        child: Text(
-                          "Sign Out",
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 248, 74, 74),
+    if (Platform.isIOS) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 247, 88, 88),
+          title: Text(
+            "Recipe App",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () => null,
+                icon: GestureDetector(
+                  child: PopupMenuButton<int>(
+                    elevation: 2,
+                    color: Color.fromARGB(173, 251, 251, 251),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        value: 1,
+                        child: Center(
+                          child: Text(
+                            "Sign Out",
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 248, 74, 74),
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                    initialValue: 0,
+                    onSelected: (value) {
+                      print("clik shod");
+                      switch (value) {
+                        case 1:
+                          {
+                            FirebaseAuth.instance.signOut();
+                            break;
+                          }
+                      }
+                    },
+                    child: const Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                      size: 26,
                     ),
-                  ],
-                  initialValue: 0,
-                  onSelected: (value) {
-                    print("clik shod");
-                    switch (value) {
-                      case 1:
-                        {
-                          FirebaseAuth.instance.signOut();
-                          break;
-                        }
-                    }
-                  },
-                  child: const Icon(
-                    Icons.more_vert,
-                    color: Colors.white,
-                    size: 26,
                   ),
+                ))
+          ],
+        ),
+        body: _pageNo[selectedPage],
+        bottomNavigationBar: ConvexAppBar.badge(
+          const {3: ''},
+          items: const [
+            TabItem(
+                icon: ImageIcon(
+                  AssetImage("assets/icons/list.png"),
+                  color: Color.fromARGB(255, 255, 255, 255),
                 ),
-              ))
-        ],
-      ),
-      body: _pageNo[selectedPage],
-      bottomNavigationBar: ConvexAppBar.badge(
-        const {3: ''},
-        items: const [
-          TabItem(
-              icon: ImageIcon(
-                AssetImage("assets/icons/list.png"),
-                color: Color.fromARGB(255, 255, 255, 255),
+                activeIcon: ImageIcon(AssetImage("assets/icons/list.png"),
+                    color: Color.fromARGB(255, 247, 88, 88)),
+                title: "My Recipes"),
+            TabItem(
+                icon: ImageIcon(
+                  AssetImage("assets/icons/home.png"),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                activeIcon: ImageIcon(AssetImage("assets/icons/home.png"),
+                    color: Color.fromARGB(255, 247, 88, 88)),
+                title: "Home"),
+            TabItem(
+                icon: ImageIcon(
+                  AssetImage("assets/icons/fave.png"),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                activeIcon: ImageIcon(AssetImage("assets/icons/fave.png"),
+                    color: Color.fromARGB(255, 247, 88, 88)),
+                title: "Favorites"),
+          ],
+          backgroundColor: Color.fromARGB(255, 247, 88, 88),
+          initialActiveIndex: selectedPage,
+          onTap: (int index) {
+            setState(() {
+              selectedPage = index;
+              print(selectedPage);
+            });
+          },
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 247, 88, 88),
+          iconTheme: IconThemeData(color: Colors.white),
+          title: Text(
+            "Recipe App",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () => null,
+                icon: GestureDetector(
+                  child: PopupMenuButton<int>(
+                    elevation: 2,
+                    color: Color.fromARGB(173, 251, 251, 251),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        value: 1,
+                        child: Center(
+                          child: Text(
+                            "Sign Out",
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 248, 74, 74),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                    initialValue: 0,
+                    onSelected: (value) {
+                      print("clik shod");
+                      switch (value) {
+                        case 1:
+                          {
+                            FirebaseAuth.instance.signOut();
+                            break;
+                          }
+                      }
+                    },
+                    child: const Icon(
+                      Icons.more_vert,
+                      color: Colors.white,
+                      size: 26,
+                    ),
+                  ),
+                ))
+          ],
+        ),
+        body: _pageNo[selectedPage],
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 247, 88, 88),
+                ),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      bottom: 8.0,
+                      left: 4.0,
+                      child: Text(
+                        "Name",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
               ),
-              activeIcon: ImageIcon(AssetImage("assets/icons/list.png"),
-                  color: Color.fromARGB(255, 247, 88, 88)),
-              title: "My Recipes"),
-          TabItem(
-              icon: ImageIcon(
-                AssetImage("assets/icons/home.png"),
-                color: Color.fromARGB(255, 255, 255, 255),
+              ListTile(
+                title: const Text('Home'),
+                leading: ImageIcon(AssetImage("assets/icons/home.png")),
+                iconColor: Colors.black,
+                tileColor: selectedPage == 1
+                    ? Color.fromARGB(116, 196, 196, 196)
+                    : Colors.transparent,
+                onTap: () {
+                  setState(() {
+                    selectedPage = 1;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              activeIcon: ImageIcon(AssetImage("assets/icons/home.png"),
-                  color: Color.fromARGB(255, 247, 88, 88)),
-              title: "Home"),
-          TabItem(
-              icon: ImageIcon(
-                AssetImage("assets/icons/fave.png"),
-                color: Color.fromARGB(255, 255, 255, 255),
+              ListTile(
+                title: const Text('My Recipes'),
+                leading: ImageIcon(AssetImage("assets/icons/list.png")),
+                iconColor: Colors.black,
+                tileColor: selectedPage == 0
+                    ? Color.fromARGB(116, 196, 196, 196)
+                    : Colors.transparent,
+                onTap: () {
+                  setState(() {
+                    selectedPage = 0;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              activeIcon: ImageIcon(AssetImage("assets/icons/fave.png"),
-                  color: Color.fromARGB(255, 247, 88, 88)),
-              title: "Favorites"),
-        ],
-        backgroundColor: Color.fromARGB(255, 247, 88, 88),
-        initialActiveIndex: selectedPage,
-        onTap: (int index) {
-          setState(() {
-            selectedPage = index;
-            print(selectedPage);
-          });
-        },
-      ),
-    );
+              ListTile(
+                title: const Text('Favorite Recipes'),
+                leading: ImageIcon(AssetImage("assets/icons/fave.png")),
+                iconColor: Colors.black,
+                tileColor: selectedPage == 2
+                    ? Color.fromARGB(116, 196, 196, 196)
+                    : Colors.transparent,
+                onTap: () {
+                  setState(() {
+                    selectedPage = 2;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
